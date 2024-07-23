0865d38c024fe9ef5e35217b2186e4d70577a441
Michele Balazs <MicheleBalazs@northkent.ac.uk>
2024-07-23 11:13:03 +0100

    initial

---
+++ app/frontend/src/components/Answer/AnswerIcon.tsx
@@ -0,0 +1,5 @@
+import { EmojiSparkle28Filled } from "@fluentui/react-icons";
+
+export const AnswerIcon = () => {
+    return <EmojiSparkle28Filled primaryFill={"rgba(18, 59, 182,1)"} aria-hidden="true" aria-label="Answer logo" />;
+};
---
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -0,0 +1,32 @@
+import { Example } from "./Example";
+
+import styles from "./Example.module.css";
+
+const DEFAULT_EXAMPLES: string[] = [
+    "write an email to a parent  explaining the college position on AI?",
+    "I am a new member of staff. Please explain the process of organising an educational visit for my students",
+    "Can you summarise the course structure of L3 Equine?"
+];
+
+const GPT4V_EXAMPLES: string[] = [
+    "Compare the impact of interest rates and GDP in financial markets.",
+    "What is the expected trend for the S&P 500 index over the next five years? Compare it to the past S&P 500 performance",
+    "Can you identify any correlation between oil prices and stock market trends?"
+];
+
+interface Props {
+    onExampleClicked: (value: string) => void;
+    useGPT4V?: boolean;
+}
+
+export const ExampleList = ({ onExampleClicked, useGPT4V }: Props) => {
+    return (
+        <ul className={styles.examplesNavList}>
+            {(useGPT4V ? GPT4V_EXAMPLES : DEFAULT_EXAMPLES).map((question, i) => (
+                <li key={i}>
+                    <Example text={question} value={question} onClick={onExampleClicked} />
+                </li>
+            ))}
+        </ul>
+    );
+};
